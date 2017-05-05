7d28a10c6a25659c04a66a2171b9bd4f05e756dd
Dídac Sabatés <sabatesduran@gmail.com>
2017-05-05 18:02:45 +0200

    Quick fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Registering ServiceWorker
 if ( 'serviceWorker' in navigator ) {
-  navigator.serviceWorker.register( '/sw.js' ).then(function(registration) {
+  navigator.serviceWorker.register( 'sw.js' ).then(function(registration) {
     // Registration was successful
     console.log( 'ServiceWorker registration successful. Scope: ' + registration.scope )
   }).catch(function(err) {
