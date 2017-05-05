845afd00304d94ecebd57ce74b382710dc5b3426
Dídac Sabatés <sabatesduran@gmail.com>
2017-05-05 18:47:22 +0200

    Add battery information

--- js/battery.js
+++ js/battery.js
@@ -1,1 +1,46 @@
-console.dir(navigator.getBattery());
+navigator.getBattery().then(function(battery) {
+  function updateAllBatteryInfo(){
+    updateChargeInfo();
+    updateLevelInfo();
+    updateChargingInfo();
+    updateDischargingInfo();
+  }
+  updateAllBatteryInfo();
+
+  battery.addEventListener('chargingchange', function(){
+    updateChargeInfo();
+  });
+  function updateChargeInfo(){
+    console.log("Battery charging? "
+                + (battery.charging ? "Yes" : "No"));
+    document.querySelector('#charging').innerText = battery.charging ? "Yes" : "No";
+  }
+
+  battery.addEventListener('levelchange', function(){
+    updateLevelInfo();
+  });
+  function updateLevelInfo(){
+    console.log("Battery level: "
+                + battery.level * 100 + "%");
+    document.querySelector('#level').innerText = `${battery.level * 100}%`;
+  }
+
+  battery.addEventListener('chargingtimechange', function(){
+    updateChargingInfo();
+  });
+  function updateChargingInfo(){
+    console.log("Battery charging time: "
+                 + battery.chargingTime + " seconds");
+    document.querySelector('#charging-time').innerText = `${battery.chargingTime / 60 } minutes`;
+  }
+
+  battery.addEventListener('dischargingtimechange', function(){
+    updateDischargingInfo();
+  });
+  function updateDischargingInfo(){
+    console.log("Battery discharging time: "
+                 + battery.dischargingTime + " seconds");
+    document.querySelector('#discharging-time').innerText = `${battery.dischargingTime / 60 } minutes`;
+  }
+
+});
